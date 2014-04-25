76a51478f771453dce4af4f024a5a41486246695
Rui Carmo <rui.carmo@gmail.com>
2014-04-25 16:32:29 +0100

    refactoring in progress

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ Created by: Rui Carmo
 License: MIT (see LICENSE.md for details)
 """
 
-import os, sys, json, logging
-logging.getLogger().setLevel(logging.DEBUG)
+import os, sys, json, logging, gettext
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 # Make sure our bundled libraries take precedence
 sys.path.insert(0,os.path.join(os.path.dirname(os.path.abspath(__file__)),'lib'))
@@ -19,6 +20,11 @@ from config import settings
 
 bottle.TEMPLATE_PATH = [os.path.join("themes", settings.theme, "views")]
 bottle.DEBUG = True
+
+# Initialize translations
+english = gettext.translation('yaki', os.path.join(os.path.dirname(__file__), 'locale'), languages=['en'])
+english.install()
+log.info(_('translation_applied'))
 
 import api, routes, controllers, tasks
 
